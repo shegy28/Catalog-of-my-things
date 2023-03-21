81ff63e1a9b6aa8ba97c3d6f6413a1a308316d75
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-03-21 16:05:52 +0100

    Add a spec_helper file

---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require_relative '../Ruby_classes/author'
+require_relative '../Ruby_classes/items'
+require_relative '../Ruby_classes/game'
+require_relative '../Ruby_classes/author'
+
+require 'yaml'
+require 'date'
