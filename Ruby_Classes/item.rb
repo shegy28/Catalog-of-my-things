2a88299dda6e7f3b4c918743d446c136b69a2332
Alex Ogeto <alexogeto54@gmail.com>
2023-03-21 18:46:12 +0300

    rename items

---
+++ Ruby_Classes/item.rb
@@ -0,0 +1,26 @@
+require 'date'
+class Item
+  attr_accessor :id, :author, :genre, :source, :publish_date
+
+  def initialize(publish_date)
+    @id = Random.rand(1..1000)
+    @publish_date = publish_date
+    @archive = false
+  end
+
+  def can_be_archived?
+    current_year = Date.today.year
+    date = Date._parse(@publish_date)
+    publish_year = current_year - date[:year]
+    publish_year > 10
+  end
+
+  def genre=(genre)
+    @genre = genre
+    @genre.add_item(self) unless @genre.items.include?(self)
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+end
