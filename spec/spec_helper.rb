274cf33c507346d72deaefa2f67e1c9ce8ed3e1d
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-03-21 16:08:05 +0100

    Fix rubocop lint errors

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 require_relative '../Ruby_classes/author'
 require_relative '../Ruby_classes/items'
 require_relative '../Ruby_classes/game'
-require_relative '../Ruby_classes/author'
 
 require 'yaml'
 require 'date'
