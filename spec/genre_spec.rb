dd9a11ecf25f17abd4b387ab8c8362d7ef283f6b
Alex Ogeto <alexogeto54@gmail.com>
2023-03-22 14:10:07 +0300

    Add test for music_genre

---
+++ spec/genre_spec.rb
@@ -0,0 +1,17 @@
+require './Ruby_Classes/music_genre'
+require './Ruby_Classes/item'
+
+describe Genre do
+  context 'It should create genre and add items' do
+    test_genre = Genre.new('Comedy')
+    it 'Should return a Genre class instance' do
+      expect(test_genre.name).to eq 'Comedy'
+    end
+
+    it 'should add the input item to the collection of items' do
+      item = Item.new(id: nil, publish_date: Time.new(2021, 1, 1))
+      test_genre.add_item(item)
+      expect(test_genre.items.length).to eq 1
+    end
+  end
+end
