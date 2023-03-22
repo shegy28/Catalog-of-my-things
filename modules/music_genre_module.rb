5efb88a3d1be86cf1881765e6c0fc6a22de83918
Alex Ogeto <alexogeto54@gmail.com>
2023-03-22 11:43:25 +0300

    Add genre module

---
+++ modules/music_genre_module.rb
@@ -0,0 +1,22 @@
+require_relative '../Ruby_Classes/music_genre'
+require'json'
+
+module MusicGenresDataController
+  def retrieve_genres
+    if File.exist?('./json_data_files/genres.json') && File.read('./json_data_files/genres.json') != ''
+      JSON.parse(File.read('./json_data_files/genres.json')).map do |genre|
+        Genre.new(name: genre['name'])
+      end
+    else
+      []
+    end
+  end
+
+  def save_genres
+    data = []
+    @genres.each do |genre|
+      data.push({ name: genre.name })
+    end
+    open('./json_data_files/genre.json', 'w') { |f| f << JSON.generate(data) }
+  end
+end
